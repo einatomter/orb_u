2c122d0e3300bb77a864914eb77cefa0d2d02eee
einatomter <einatomter@gmail.com>
2023-02-09 12:10:10 +0100

    reduce min distance for imu init (0.01 -> 0.005)

--- orb_slam3/src/LocalMapping.cc
+++ orb_slam3/src/LocalMapping.cc
@@ -284,7 +284,7 @@ void LocalMapping::Run()
                             mTinit += mpCurrentKeyFrame->mTimeStamp - mpCurrentKeyFrame->mPrevKF->mTimeStamp;
                         if(!mpCurrentKeyFrame->GetMap()->GetIniertialBA2())
                         {
-                            if((mTinit<10.f) && (dist<0.01))
+                            if((mTinit<10.f) && (dist<0.005))
                             {
                                 cout << "Not enough motion for initializing. Reseting..." << endl;
                                 unique_lock<mutex> lock(mMutexReset);
